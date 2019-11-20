2290c05be474a4d72e06b7c92a13148856a2c80b
Xingchen <xczhao007@gmail.com>
2019-11-20 14:52:30 -0600

    Finish connect method. Start acquisite method.

--- dso6032.py
+++ dso6032.py
@@ -12,7 +12,7 @@ import numpy as np
 
 class DSO6034A(object):
     def __init__(self, points, visa_address, name, directory, visa_dir="C:\\Windows\\System32\\visa32.dll", timeout=10000):
-        self.USER_REQUESTED_POINTS = points
+        self.USER_REQUESTED_POINTS = int(points)
         self.VISA_ADDRESS = visa_address
         self.GLOBAL_TIMEOUT = timeout
         self.FILE_NAME = name
@@ -21,17 +21,18 @@ class DSO6034A(object):
 
     def connect(self):
         """
-        Define and open the scape by the VISA address. This uses PyVISA
+        Define and open the scope by the VISA address. This uses PyVISA
+        :return: scope object
         """
         rm = visa.ResourceManager(self.VISA_DIR)
         try:
-            dso6032a = rm.open_resource(self.VISA_ADDRESS)
+            scope = rm.open_resource(self.VISA_ADDRESS)
         except Exception:
             print("Unable to connect to oscilloscope at {:s}. Aborting script.\n".format(self.VISA_ADDRESS))
             sys.exit()
-        dso6032a.timeout = self.GLOBAL_TIMEOUT  # Set Global Timeout
-        dso6032a.clear()  # Clear the instrument bus
-        return dso6032a
+        scope.timeout = self.GLOBAL_TIMEOUT  # Set Global Timeout
+        scope.clear()  # Clear the instrument bus
+        return scope
 
     def channels(self, scope):
         idn = str(scope.query("*IDN?"))
@@ -45,18 +46,20 @@ class DSO6034A(object):
             chs_list = [0, 0]
         else:
             chs_list = [0, 0, 0, 0]
+
         number_channels_on = 0
         """
         After the CHS_LIST array is filled it could, for example look like: if chs 1,3 and 4 were on, 
         CHS_LIST = [1,0,1,1].
         """
 
-        analog_vert_pres = np.zeros([12])
+        analog_vert_pres = np.zeros((3, 4))
         """
-        analog_vert_pres = (Y_INCrement_Ch1, Y_INCrement_Ch2, Y_INCrement_Ch3, Y_INCrement_Ch4, 
-                            Y_ORIGin_Ch1, Y_ORIGin_Ch2, Y_ORIGin_Ch3, Y_ORIGin_Ch4, 
-                            Y_REFerence_Ch1, Y_REFerence_Ch2, Y_REFerence_Ch3, Y_REFerence_Ch4)
+        analog_vert_pres = [[Y_INCrement_Ch1, Y_INCrement_Ch2, Y_INCrement_Ch3, Y_INCrement_Ch4], 
+                            [Y_ORIGin_Ch1, Y_ORIGin_Ch2, Y_ORIGin_Ch3, Y_ORIGin_Ch4], 
+                            [Y_REFerence_Ch1, Y_REFerence_Ch2, Y_REFerence_Ch3, Y_REFerence_Ch4]]
         """
+
         ch_units = ["BLANK", "BLANK", "BLANK", "BLANK"]
         scope.write(":WAVeform:POINts:MODE MAX")
         ch_index = 1  # Channel index
@@ -72,5 +75,84 @@ class DSO6034A(object):
             else:
                 chs_list[ch-1] = 1
                 number_channels_on += 1
+                pre_amble = scope.query(":WAVeform:PREamble?").split(",")
+                analog_vert_pres[0, ch-1] = float(pre_amble[7])
+                analog_vert_pres[1, ch-1] = float(pre_amble[8])
+                analog_vert_pres[2, ch-1] = float(pre_amble[9])
+                ch_units[ch-1] = str(scope.query(":CHANnel{:s}:UNITs?".format(ch)).strip('\n'))
+            ch_index += 1
+
+        if number_channels_on == 0:
+            scope.clear()
+            scope.close()
+            sys.exit("No data has been acquired. Properly closing scope and aborting script.")
+
+        chs_on = []
+        ch_index = 1
+        for ch in chs_list:
+            if ch == 1:
+                chs_on.append(int(ch))
+            ch_index += 1
+        return chs_on
+
+    def acquisite(self, scope, channels):
+        """
+        Setup data export - For repetitive acquisitions, this only needs to be done once unless settings are changed
+        :param scope: scope object returned by 'connect' method.
+        :return:
+        """
+        scope.write(":WAVeform:FORMat WORD")
+        scope.write(":WAVeform:BYTeorder LSBFirst")
+        scope.write(":WAVeform:UNSigned 0")
+
+        acq_type = str(scope.query(":ACQuire:TYPE?")).strip("\n")
+        """
+        This can also be done when pulling pre-ambles (pre[1]) or may be known ahead of time, 
+        but since the script is supposed to find everything, it is done now.
+        """
+
+        if acq_type == "AVER" or acq_type == "HRES":
+            points_mode = "NORMal"
+            """
+            If the :WAVeform:POINts:MODE is RAW, and the Acquisition Type is Average, the number of points 
+            available is 0. If :WAVeform:POINts:MODE is MAX, it may or may not return 0 points.
+            If the :WAVeform:POINts:MODE is RAW, and the Acquisition Type is High Resolution, then the effect 
+            is (mostly) the same as if the Acq. Type was Normal (no box-car averaging).
+            Note: if you use :SINGle to acquire the waveform in AVERage Acq. Type, no average is performed, 
+            and RAW works. See sample script "InfiniiVision_2_Simple_Synchronization_Methods.py"
+            """
+        else:
+            points_mode = "RAW"
+
+        """
+        Note:
+        :WAVeform:POINts:MODE RAW corresponds to saving the ASCII XY or Binary data formats to a USB stick on the scope
+        :WAVeform:POINts:MODE NORMal corresponds to saving the CSV or H5 data formats to a USB stick on the scope
+        
+        Find max points for scope as is, ask for desired points, find how many points will actually be returned.
+        KEY POINT: the data must be on screen to be retrieved.  If there is data off-screen, :WAVeform:POINts? will 
+        not "see it."
+        Addendum 1 shows how to properly get all data on screen, but this is never needed for Average and High 
+        Resolution Acquisition Types, since they basically don't use off-screen data; what you see is what you get.
+        """
+        scope.write(":WAVeform:SOURce CHANnel{:s}".format(channels[0]))
+        scope.write(":WAVeform:POINts MAX")
+        scope.write(":WAVeform:POINts:MODE {:s}".format(points_mode))
+        max_currently_available_points = int(scope.query(":WAVeform:POINts?"))
+
+        #  The scope will return a -222,"Data out of range" error if fewer than 100 points are requested,
+        #  even though it may actually return fewer than 100 points.
+        if self.USER_REQUESTED_POINTS < 100:
+            self.USER_REQUESTED_POINTS = 100
+        if self.USER_REQUESTED_POINTS > max_currently_available_points or acq_type == "PEAK":
+            self.USER_REQUESTED_POINTS = max_currently_available_points
+
+        # If one wants some other number of points...
+        # Tell it how many points you want
+        scope.write(":WAVeform:POINts " + str(self.USER_REQUESTED_POINTS))
+        number_of_points_to_actually_retrieve = int(scope.query(":WAVeform:POINts?"))
+
+
+
 
 
---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a1 = np.zeros((3, 4))
+a2 = np.arange(0, 12).reshape((3, 4))
+print(a1[0, :])
+print(a2[0, :])
