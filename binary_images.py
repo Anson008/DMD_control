c4777bc48b926842d9d30d952b4322b827d9bc75
Anson008 <xczhao007@gmail.com>
2019-12-06 13:43:32 -0600

    Add scale attribute and saving to file methods

--- binary_images.py
+++ binary_images.py
@@ -1,10 +1,11 @@
 import numpy as np
 import time
 import cv2
+import os
 
 
 class BinaryImage(object):
-    def __init__(self, frames, width, height, periods):
+    def __init__(self, frames, width, height, xy_scale, periods):
         """
         :_frames: int, number of frames in the image sequence
         :_width: int, image width
@@ -15,6 +16,7 @@ class BinaryImage(object):
         self._frames = frames
         self._width = width
         self._height = height
+        self._xy_scale = xy_scale
         self._periods = periods
         self._pixels = None
 
@@ -22,21 +24,24 @@ class BinaryImage(object):
         """
         :return: tuple, the shape of image sequence in (frames, height, width).
         """
-        return self._frames, self._height, self._width
+        return self._frames, self._height, self._width, self._xy_scale
 
     def get_pixels(self):
+        """
+        :return: numpy array, pixel values of the image sequence
+        """
         return self._pixels
 
     def generate_images(self):
         """
         Generate binary images according to the given periods.
         """
-        self._pixels = np.zeros((self._frames, self._height, self._width), dtype=np.uint8)
+        self._pixels = np.zeros((self._frames, self._height * self._xy_scale, self._width * self._xy_scale), dtype=np.uint8)
         for z in range(self._frames):
-            for y in range(self._height):
-                for x in range(self._width):
+            for y in range(self._height * self._xy_scale):
+                for x in range(self._width * self._xy_scale):
                     # first define the mapping between the index of period value and (y, x)
-                    i_period = x + self._width * y
+                    i_period = (x // self._xy_scale) + self._width * (y // self._xy_scale)
                     if (z // self._periods[i_period]) % 2 == 0:
                         self._pixels[z][y][x] = 255
                     else:
@@ -53,10 +58,39 @@ class BinaryImage(object):
                 print(self._pixels[:, y, x])
         print("-"*10, "End of printing", "-"*10, "\n")
 
-    def make_binary_video(self):
-        fps, w, h = 1, self._width, self._height
+    def preview(self):
+        for z in range(self._frames):
+            cv2.imshow('Preview of images', self._pixels[z, :, :])
+            if cv2.waitKey(0) == ord('q'):
+                break
+        cv2.destroyAllWindows()
+
+    def make_image_sequence(self, directory='./img_sequence', fmt='.png'):
+        """
+        Save image sequence to a user specified directory, with auto naming from "0" to "# of images - 1".
+        :para directory: string, destination directory.
+        :para fmt: string, format of the image files.
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        for z in range(self._frames):
+            file_path = directory + "/" + "{:d}".format(z).zfill(len(str(z))) + fmt
+            cv2.imwrite(file_path, self._pixels[z, :, :])
+
+    def make_binary_video(self, fps, directory='./videos', filename='video1', fmt='.avi'):
+        """
+        Save image sequence as a video to user specified directory.
+        :para fps: int, frame rate of the video.
+        :para directory: string, destination directory.
+        :para filename: string, file name of the video, without extension.
+        :para fmt: string, format of the image files.
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        w, h = self._width * self._xy_scale, self._height * self._xy_scale
+        file_path = directory + '/' + filename + fmt
         fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        video = cv2.VideoWriter('./binary_image.avi', fourcc, fps, (w, h), False)
+        video = cv2.VideoWriter(file_path, fourcc, fps, (w, h), False)
         for z in range(self._frames):
             thresh, frame_b = cv2.threshold(self._pixels[z, :, :], 127, 255, cv2.THRESH_BINARY)
             video.write(frame_b)
@@ -76,20 +110,24 @@ if __name__ == "__main__":
                  419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
                  467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
 
-    binary_img = BinaryImage(240, 2, 2, prime_num)
-    frames, height, width = binary_img.get_img_shape()
+    binary_img = BinaryImage(240, 2, 2, 100, prime_num)
+    frames, height, width, scale = binary_img.get_img_shape()
 
     start = time.time()
     binary_img.generate_images()
     print("\nIt took {:.2f} s to generate image sequence of shape "
-          "({:d}, {:d}, {:d}) (frames, height, width).\n".format(time.time() - start, frames, height, width))
+          "({:d}, {:d}, {:d}). \nThe actual height and width are scaled by a factor of {:d}."
+          "\nThe shape of actual images should be ({:d}, {:d}, {:d})."
+          .format(time.time() - start, frames, height, width, scale, frames, height * scale, width * scale))
 
+    # binary_img.preview()
     # binary_img.print_through_time([(0, 2), (0, 2)])
 
-    # image = binary_img.get_pixels()
-    # print("Shape of image:", image.shape)
+    image = binary_img.get_pixels()
+    print("\nShape of generated images:", image.shape)
 
-    binary_img.make_binary_video()
+    #binary_img.make_image_sequence()
+    #binary_img.make_binary_video(fps=24)
 
 
 
