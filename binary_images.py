49373c1d51ff23187fca157f61a863825fd8fa11
Anson008 <xczhao007@gmail.com>
2019-12-09 15:21:55 -0600

    Add PeriodGenerator class in a seperated file

--- binary_images.py
+++ binary_images.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import cv2
 import os
+from periods import PeriodGenerator
 
 
 class BinaryImage(object):
@@ -114,16 +115,8 @@ class BinaryImage(object):
 
 if __name__ == "__main__":
     # Leading 100 prime numbers
-    prime_num = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-                 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-                 73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
-                 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
-                 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
-                 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-                 283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
-                 353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
-                 419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
-                 467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
+    periods = PeriodGenerator()
+    prime_num = periods.prime_numbers()
 
     padding = ((200, 200), (200, 200))
     binary_img = BinaryImage(24, 2, 2, 100, prime_num)
@@ -140,7 +133,7 @@ if __name__ == "__main__":
 
     binary_img.add_padding(pad_width=padding)
     # binary_img.undo_padding(pad_width=padding)
-    # binary_img.preview()
+    binary_img.preview()
     image = binary_img.get_pixels()
     print("\nShape of generated images:", image.shape)
 
