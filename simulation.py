7dbae58f842671a76884701492a3d1105a5d3d63
Anson008 <xczhao007@gmail.com>
2019-12-11 16:31:39 -0600

    Define 'SimulationEngine' class
    Add 'freq_map' method to create {freq: index} map.
    Add 'do_fft' method to perform FFT on data.
    Add 'test*.py' to .gitignore.

--- simulation.py
+++ simulation.py
@@ -5,16 +5,52 @@ from periods import PeriodGenerator
 
 
 class SimulationEngine:
-    pass
+    def __init__(self, t_step, pattern_shape=(2, 2)):
+        self._t_step = t_step
+        self._data = None
+        self._freq_to_index = dict()
+        self._pattern_shape = pattern_shape
 
+    def freq_map(self):
+        period = PeriodGenerator
+        num_pixels = self._pattern_shape[0] * self._pattern_shape[1]
+        p_num = period.prime_numbers()[:num_pixels]
+        for i in range(num_pixels):
+            freq = 1 / (self._t_step * p_num[i] * 2)
 
+            # x = index % width, y = index // width
+            y, x = divmod(i, self._pattern_shape[1])
+            self._freq_to_index[freq] = (x, y)
 
+    def get_freq_map(self):
+        return self._freq_to_index
+
+    def load_data(self, data_path='./raw_data/test.npy'):
+        self._data = np.load(data_path)
+
+    def get_data_shape(self):
+        return self._data.shape
+
+    def do_fft(self):
+        avg = np.mean(self._data, axis=(1, 2))
+        ft = np.fft.fft(avg)
+        n = avg.size
+        freq = np.sort(np.fft.fftfreq(n, d=self._t_step))
+        return ft, freq
 
 
 if __name__ == "__main__":
-    periods = PeriodGenerator()
-    prime_num = periods.prime_numbers()
-    binary_images = BinaryImage(24, 2, 2, 100)
-    frame, width, height, scale = binary_images.get_img_shape()
+    simEng = SimulationEngine(0.1)
+    simEng.load_data()
+    frame, width, height = simEng.get_data_shape()
+    print("({:d}, {:d}, {:d})".format(frame, width, height))
+
+    ft, freq = simEng.do_fft()
+    plt.plot(freq, ft.real)
+    plt.show()
+
+    simEng.freq_map()
+    f_map = simEng.get_freq_map()
+    print(f_map)
 
 
