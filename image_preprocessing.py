90719b58e35ab76754f9af3f408398291a7a0114
Anson008 <xczhao007@gmail.com>
2019-12-16 14:38:13 -0600

    Add 'read_video' method

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -20,6 +20,27 @@ def read_images(path='E:/Data_exp/imaging_T_60fps_8X8_4min_files'):
     return img
 
 
+def read_video(path='E:/Data_exp/image_T_8X8_60fps.avi'):
+    cap = cv2.VideoCapture(path)
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print("Number of frames in the video: {:d}".format(frame_count))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    res = np.empty((frame_count, frame_height, frame_width), np.dtype('uint8'))
+
+    count = 0
+    ret = True
+    while count < frame_count and ret:
+        ret, frame = cap.read()
+        res[count] = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        count += 1
+    cap.release()
+    return res
+
+
 def save_image_to_npy(data, path="E:/code_xz/DMD_control/raw_data/",
                       filename='T_60fps_8X8_4min.npy'):
     np.save(path + filename, data)
@@ -36,10 +57,12 @@ def preview(data):
 if __name__ == "__main__":
     # test_path = 'E:/Data_exp/test_img'
     start = time.time()
-    images = read_images(path='E:/Data_exp/60fps_reference_24fps_8X8_sample_2min_files')
+    # images = read_images(path='E:/Data_exp/60fps_reference_24fps_8X8_sample_2min_files')
+    images = read_video()
     # print("Image shape:", images.shape)
-    save_image_to_npy(images, filename='60fps_reference_24fps_8X8_sample_2min_files.npy')
+    save_image_to_npy(images, filename='image_T_8X8_60fps.npy')
     end = time.time()
     print("It took {:.2f} seconds to convert the image data to .npy format.".format(end - start))
+
 
 
